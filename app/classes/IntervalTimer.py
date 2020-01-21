623e5f3cad0fd52a25b8e2b4698eaf51e9ae1ffc
acap <senieck@gmail.com>
2020-01-22 00:06:13 +0300

    renamed classes correctly

--- app/classes/IntervalTimer.py
+++ app/classes/IntervalTimer.py
@@ -1,19 +1,23 @@
 
 from pymongo.collection import Collection
 import time
-from .Crawler import HackerNewsCrawler
+from .HackerNewsCrawler import HackerNewsCrawler
 
 
 class IntervalTimer:
     @staticmethod
-    def start_scanning(m_coll, interval=1200):
+    def start_scanning(m_coll, interval=1200, repeated=True):
         """
         Scanning thread entrypoint. Default scanning interval is 30m (1800s)
         """
 
         crawler = HackerNewsCrawler(m_coll)
-        while True:
+
+        if repeated:
+            while True:
+                crawler.scan()
+                time.sleep(interval)
+        else:
             crawler.scan()
-            time.sleep(interval)
 
 
